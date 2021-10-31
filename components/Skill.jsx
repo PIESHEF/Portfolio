0d7727d04d23bf1362aae0f421d9a3ea81c93918
Alex Pieshefski <apieshefski@gmail.com>
2021-10-31 17:44:47 -0400

    Styled skill icons

--- components/Skill.jsx
+++ components/Skill.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class Skill extends React.Component {
   render(props) {
     return (
-      <div>
+      <div className="box-border h-32 w-32 p-8 rounded-lg shadow-lg bg-gray-100">
         <img alt={this.props.alt} width="100px" src={this.props.src} />
       </div>
     );
